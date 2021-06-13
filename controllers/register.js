60aa40ff590b4dcfbb379e3156d3228c37f54ac8
Cyberproton <cyberprotonAtMailbox@gmail.com>
2021-06-13 23:55:39 +0700

    update register api

--- controllers/register.js
+++ controllers/register.js
@@ -1,8 +1,10 @@
+const Profile = require('../models/Profile')
 const User = require('../models/User')
 
 async function register(req, res, next) {
     let username = req.body.username
     let password = req.body.password
+    const phone = req.body.phone
 
     if (!username || !password) {
         res.status(400).json({ message: 'Username or Password is invalid' })
@@ -16,7 +18,19 @@ async function register(req, res, next) {
         return
     }
 
-    let user = new User({ _id: username, password: password })
+    let profile = undefined
+    if (phone) {
+        profile = new Profile({ phone: phone })
+    }
+
+    let data = {}
+    data._id = username
+    data.password = password
+    if (profile) {
+        data.profile = profile
+    }
+
+    let user = new User(data)
     user.save((err, user) => {
         if (err) {
             res.status(500).json({ message: 'Could not create your account', error: err.message })
