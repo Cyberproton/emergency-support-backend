f3e4e0f9977a23dba2d908a2214381959ea5c3ed
Cyberproton <cyberprotonAtMailbox@gmail.com>
2021-06-13 15:30:54 +0700

    update location api

--- controllers/location.js
+++ controllers/location.js
@@ -37,3 +37,45 @@ exports.postLocation = (req, res, next) => {
         }
     }).exec()
 }
+
+exports.getYourLocation = (req, res, next) => {
+    const username = req.jwtDecoded.user
+
+    if (!username) {
+        res.status(401).json({ message: 'Could not authorized' })
+        return
+    }
+
+    User
+        .findById(username)
+        .exec()
+        .then(user => {
+            res.status(200).json({ message: 'Found your location', location: user.currentLocation })
+        })
+        .catch(err => res.status(500).json({ message: 'Something wrong happened', error: err.message }))
+}
+
+exports.getUserLocation = (req, res, next) => {
+    const username = req.jwtDecoded.user
+    const userId = req.params.id
+
+    if (!username) {
+        res.status(401).json({ message: 'Could not authorized' })
+        return
+    }
+
+    if (!userId) {
+        res.status(400).json({ message: 'Could not find user' })
+        return
+    }
+
+    User
+        .findById(userId)
+        .exec()
+        .then(user => {
+            res.status(200).json({ message: 'Found user location', location: user.currentLocation })
+        })
+        .catch(err => 
+            res.status(500).json({ message: 'Something wrong happened', error: err.message })
+        )
+}
--- routes/location.js
+++ routes/location.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const { postLocation } = require('../controllers/location')
+const { postLocation, getYourLocation, getUserLocation } = require('../controllers/location')
 const router = express.Router()
 
 router.route('/').post(postLocation)
+router.route('/').get(getYourLocation)
+router.route('/:id').get(getUserLocation)
 
 module.exports = router
