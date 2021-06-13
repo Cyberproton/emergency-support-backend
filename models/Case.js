1a26fbe38c3e0cd782c51490b3f2c0a073fe0f40
Cyberproton <cyberprotonAtMailbox@gmail.com>
2021-06-13 15:32:03 +0700

    update case model

--- models/Case.js
+++ models/Case.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const help_case = new Schema({
-    caller: { type: String, required: true },
-    volunteers: [ String ],
+    caller: { type: String, required: true, ref: 'user' },
+    volunteers: [ { type: String, ref: 'user' } ],
     is_closed: { type: Boolean, default: false },
-    location: { type: String, default: '' }
+    location: { type: String, default: '' },
+    searchRadius: { type: Number, default: 5000 }
 })
 
 module.exports = mongoose.model('help_case', help_case)
